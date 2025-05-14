f1b55091479752a56370abc4107878b6400b2e3f
Tommertom <Tom.Gruintjes@gmail.com>
2025-05-14 15:04:32 +0200

    placeholders for missing tools

--- src/index.ts
+++ src/index.ts
@@ -27,18 +27,6 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
   return {
     tools: [
       {
-        name: "calculate_sum",
-        description: "A useful tool to add two numbers",
-        inputSchema: {
-          type: "object",
-          properties: {
-            a: { type: "number" },
-            b: { type: "number" },
-          },
-          required: ["a", "b"],
-        },
-      },
-      {
         name: "get_ionic_component_definition",
         description:
           "A useful tool to get the component definition of an Ionic component (all html elements starting with ion-)",
@@ -70,23 +58,32 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
           "A useful tool to get all Ionic elements (all html elements starting with ion-)",
         inputSchema: {
           type: "object",
-          properties: {
-            html_tag: { type: "string" },
-          },
-          required: ["html_tag"],
+          properties: {},
+          required: [],
         },
       },
 
       {
         name: "get_all_ionic_templates",
         description:
-          "A useful tool to get templates for usefull app screens using Ionic elements (all html elements starting with ion-)",
+          "A useful tool to get list of available templates for usefull app screens using Ionic elements (all html elements starting with ion-)",
+        inputSchema: {
+          type: "object",
+          properties: {},
+          required: [],
+        },
+      },
+
+      {
+        name: "get_specific_ionic_template",
+        description:
+          "A useful tool to get example template with Ionic elements",
         inputSchema: {
           type: "object",
           properties: {
             html_tag: { type: "string" },
           },
-          required: ["html_tag"],
+          required: ["template_name"],
         },
       },
     ],
@@ -95,19 +92,6 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
 
 // Handle tool execution
 server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  if (request.params.name === "calculate_sum") {
-    //@ts-ignore
-    const { a, b } = request.params.arguments;
-    return {
-      content: [
-        {
-          type: "text",
-          text: String(a + b),
-        },
-      ],
-    };
-  }
-
   if (request.params.name === "get_ionic_component_definition") {
     //@ts-ignore
     const { html_tag } = request.params.arguments;
@@ -126,6 +110,79 @@ server.setRequestHandler(CallToolRequestSchema, async (request) => {
       ],
     };
   }
+
+  if (request.params.name === "get_ionic_component_examples") {
+    //@ts-ignore
+    const { html_tag } = request.params.arguments;
+    let result = null;
+    try {
+      result = giveIonicDefinition(html_tag);
+    } catch (e) {
+      result = { error: "Failed to read or parse core.json" };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result, null, 2),
+        },
+      ],
+    };
+  }
+
+  if (request.params.name === "get_all_ionic_components") {
+    let result = null;
+    try {
+      result = "No components avaible yet";
+    } catch (e) {
+      result = { error: "Failed to read or parse core.json" };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result, null, 2),
+        },
+      ],
+    };
+  }
+
+  if (request.params.name === "get_all_ionic_templates") {
+    let result = null;
+    try {
+      result = "No templates available yet";
+    } catch (e) {
+      result = { error: "Failed to get templates" };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result, null, 2),
+        },
+      ],
+    };
+  }
+
+  if (request.params.name === "get_specific_ionic_template") {
+    //@ts-ignore
+    const { template_name } = request.params.arguments;
+    let result = null;
+    try {
+      result = `No template found for ${template_name}`;
+    } catch (e) {
+      result = { error: "Failed to get specific template" };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result, null, 2),
+        },
+      ],
+    };
+  }
+
   throw new Error("Tool not found");
 });
 
